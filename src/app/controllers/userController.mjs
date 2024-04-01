fc11f8e66c780db536f45571a9ee47d347dcc806
Dedek Tegar Apriyandi <dedektegarr@gmail.com>
2024-04-01 22:07:03 +0700

    feat: create endpoint for update user point

--- src/app/controllers/userController.mjs
+++ src/app/controllers/userController.mjs
@@ -1,3 +1,5 @@
+import User from "../models/User.mjs";
+
 const userController = {
   currentUser: async (req, res) => {
     res.status(200).send({
@@ -12,6 +14,32 @@ const userController = {
         user: req.user,
       },
     });
+  },
+  updatePoint: async (req, res) => {
+    try {
+      const updatePoint = await User.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          point: req.body.point,
+        },
+        { new: true }
+      );
+
+      if (!updatePoint) throw new Error("Gagal menambah point");
+
+      return res.status(200).send({
+        meta: {
+          code: 200,
+          status: "success",
+          message: "Point berhasil diperbaharui",
+        },
+        data: {
+          user: updatePoint,
+        },
+      });
+    } catch (error) {
+      return res.status(400).send(error.message);
+    }
   },
 };
 
--- src/routes/user/user.mjs
+++ src/routes/user/user.mjs
@@ -4,5 +4,6 @@ import userController from "../../app/controllers/userController.mjs";
 const userRouter = Router();
 
 userRouter.get("/", userController.currentUser);
+userRouter.patch("/:id/point", userController.updatePoint);
 
 export default userRouter;
